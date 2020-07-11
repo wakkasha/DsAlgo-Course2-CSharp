db0a1ba0a478c248da3ef574bda27ee052276007
Shawn <shawnwalker.net@gmail.com>
2020-07-11 02:56:45 -0700

    Added insert function to tree

---
+++ BinarySearchTree/Tree.cs
@@ -0,0 +1,57 @@
+﻿namespace DsAlgo_Course2_CSharp.BinarySearchTree
+{
+	public class Tree
+	{
+		private class Node
+		{
+			public int value;
+			public Node leftChild;
+			public Node rightChild;
+
+			public Node(int value)
+			{
+				this.value = value;
+			}
+
+			public override string ToString() => $"Node={value}";
+		}
+
+		private Node root;
+
+		public void Insert(int value)
+		{
+			var node = new Node(value);
+
+			if (root is null)
+			{
+				root = node;
+				return;
+			}
+
+			var current = root;
+			while (true)
+			{
+				if (value < current.value)
+				{
+					if (current.leftChild is null)
+					{
+						current.leftChild = node;
+						break;
+					}
+					current = current.leftChild;
+				}
+				else
+				{
+					if (current.rightChild is null)
+					{
+						current.rightChild = node;
+						break;
+					}
+					current = current.rightChild;
+				}
+			}
+		}
+
+		// find
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using DsAlgo_Course2_CSharp.BinarySearchTree;
+using System;
 
 namespace DsAlgo_Course2_CSharp
 {
-	class Program
+	internal class Program
 	{
-		static void Main(string[] args)
+		private static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			#region Tree
+
+			Tree tree = new Tree();
+			tree.Insert(7);
+			tree.Insert(4);
+			tree.Insert(9);
+			tree.Insert(1);
+			tree.Insert(6);
+			tree.Insert(8);
+			tree.Insert(10);
+
+			Console.WriteLine("");
+
+			#endregion Tree
 		}
 	}
 }
